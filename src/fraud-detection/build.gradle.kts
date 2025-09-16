33b38f1817174bb95c9e178c4c6ff154a15feca5
venkatswan1991 <venkatswan1991@gmail.com>
2025-09-16 17:21:41 +0530

    project added

---
+++ src/fraud-detection/build.gradle.kts
@@ -0,0 +1,101 @@
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.google.protobuf.gradle.*
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    kotlin("jvm") version "2.2.20"
+    application
+    id("java")
+    id("idea")
+    id("com.google.protobuf") version "0.9.5"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "io.opentelemetry"
+version = "1.0"
+
+
+val grpcVersion = "1.75.0"
+val protobufVersion = "4.32.1"
+
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+
+
+dependencies {
+    implementation("com.google.protobuf:protobuf-java:${protobufVersion}")
+    testImplementation(kotlin("test"))
+    implementation(kotlin("script-runtime"))
+    implementation("org.apache.kafka:kafka-clients:4.1.0")
+    implementation("com.google.api.grpc:proto-google-common-protos:2.61.1")
+    implementation("io.grpc:grpc-protobuf:${grpcVersion}")
+    implementation("io.grpc:grpc-stub:${grpcVersion}")
+    implementation("io.grpc:grpc-netty:${grpcVersion}")
+    implementation("io.grpc:grpc-services:${grpcVersion}")
+    implementation("io.opentelemetry:opentelemetry-api:1.54.0")
+    implementation("io.opentelemetry:opentelemetry-sdk:1.54.0")
+    implementation("io.opentelemetry:opentelemetry-extension-annotations:1.18.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.25.1")
+    implementation("org.slf4j:slf4j-api:2.0.17")
+    implementation("com.google.protobuf:protobuf-kotlin:${protobufVersion}")
+    implementation("dev.openfeature:sdk:1.17.0")
+    implementation("dev.openfeature.contrib.providers:flagd:0.11.15")
+
+    if (JavaVersion.current().isJava9Compatible) {
+        // Workaround for @javax.annotation.Generated
+        // see: https://github.com/grpc/grpc-java/issues/3633
+        implementation("javax.annotation:javax.annotation-api:1.3.2")
+    }
+}
+
+tasks {
+    shadowJar {
+        mergeServiceFiles()
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+  compilerOptions {
+    jvmTarget.set(JvmTarget.JVM_17)
+  }
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:${protobufVersion}"
+    }
+    plugins {
+
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:${grpcVersion}"
+        }
+    }
+    generateProtoTasks {
+        ofSourceSet("main").forEach {
+            it.plugins {
+                // Apply the "grpc" plugin whose spec is defined above, without
+                // options. Note the braces cannot be omitted, otherwise the
+                // plugin will not be added. This is because of the implicit way
+                // NamedDomainObjectContainer binds the methods.
+                id("grpc") { }
+            }
+        }
+    }
+}
+
+application {
+    mainClass.set("frauddetection.MainKt")
+}
+
+tasks.jar {
+    manifest.attributes["Main-Class"] = "frauddetection.MainKt"
+}
---
+++ src/shipping/src/main.rs
@@ -0,0 +1,46 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+use actix_web::{App, HttpServer};
+use opentelemetry_instrumentation_actix_web::{RequestMetrics, RequestTracing};
+use std::env;
+use tracing::info;
+
+mod telemetry_conf;
+use telemetry_conf::init_otel;
+mod shipping_service;
+use shipping_service::{get_quote, ship_order};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    match init_otel() {
+        Ok(_) => {
+            info!("Successfully configured OTel");
+        }
+        Err(err) => {
+            panic!("Couldn't start OTel: {0}", err);
+        }
+    };
+
+    let port: u16 = env::var("SHIPPING_PORT")
+        .expect("$SHIPPING_PORT is not set")
+        .parse()
+        .expect("$SHIPPING_PORT is not a valid port");
+    let addr = format!("0.0.0.0:{}", port);
+    info!(
+        name = "ServerStartedSuccessfully",
+        addr = addr.as_str(),
+        message = "Shipping service is running"
+    );
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(RequestTracing::new())
+            .wrap(RequestMetrics::default())
+            .service(get_quote)
+            .service(ship_order)
+    })
+    .bind(&addr)?
+    .run()
+    .await
+}
